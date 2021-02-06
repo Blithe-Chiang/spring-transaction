d060e588d645c8b936db0567385b9b9d50294576
Blithe-Chiang <cn18750441251@126.com>
2021-02-06 18:38:47 +0800

    测试：测试在Junit里面直接使用Dao操作数据库是否不会自动提交

--- spring-mybatis-transaction/src/test/java/com/example/AppTest.java
+++ spring-mybatis-transaction/src/test/java/com/example/AppTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import com.example.dao.GoodsDao;
+import com.example.dao.SaleDao;
+import com.example.domain.Sale;
 import com.example.service.BuyGoodsService;
 import com.example.service.impl.BuyGoodsServiceImpl;
 import org.junit.Test;
@@ -33,5 +35,21 @@ public class AppTest {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
         BuyGoodsService buyGoodsService = ctx.getBean(BuyGoodsServiceImpl.class);
         buyGoodsService.buy(1002, 2);
+    }
+
+    /**
+     * 用来测试是否只有直接在Junit里面使用dao修改数据库不会自动提交
+     * <p>
+     * 结果：
+     * 不是这样的
+     */
+    @Test
+    public void testWeatherDiffInJunit() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
+        SaleDao saleDao = ctx.getBean(SaleDao.class);
+        Sale sale = new Sale();
+        sale.setGid(1001);
+        sale.setNums(2);
+        int i = saleDao.insertSale(sale);
     }
 }
